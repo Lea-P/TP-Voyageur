a20add7b403b7bced8e865971691ccd05aff2bed
Lea <lea.pons@mines-albi.fr>
2020-06-05 17:19:33 +0200

    message

---
+++ TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationLigne.java
@@ -0,0 +1,11 @@
+package fr.emac.gipsi.gsi.animation;
+
+public class AnimationLigne extends AbstractAnimation {
+
+	@Override
+	public void runAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
