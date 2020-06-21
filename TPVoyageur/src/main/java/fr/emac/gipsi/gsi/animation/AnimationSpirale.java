5c45ac66897b40d8eb96b0af362204330df80356
Vincent <vincent.jacquot@mines-albi.fr>
2020-06-21 10:17:11 +0200

    super vitesse

--- TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationSpirale.java
+++ TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationSpirale.java
@@ -13,25 +13,24 @@ public class AnimationSpirale extends AbstractAnimation {
 		showScreen(ecranDeb);
 		ecranInt=copyScreen(ecranDeb);
 		for(int i=0;i<ecranInt.getColMax()/2+1;i++){
-			int k = ecranInt.getColMax()/2-1;
 			for(int j=i;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(i, j, ecranFin.getColorByXY(i,j));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 			for(int j=i+1;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(j, ecranInt.getColMax()-i, ecranFin.getColorByXY(j,ecranInt.getColMax()-i));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 			for(int j=i+1;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(ecranInt.getColMax()-i, ecranInt.getColMax()-j, ecranFin.getColorByXY(ecranInt.getColMax()-i,ecranInt.getColMax()-j));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 			for(int j=i+1;j<ecranInt.getColMax()-i;j++){
 				ecranInt.updateColorByXY(ecranInt.getColMax()-j, i, ecranFin.getColorByXY(ecranInt.getColMax()-j,i));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 		}
--- TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationSpirale2.java
+++ TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/AnimationSpirale2.java
@@ -19,22 +19,22 @@ public class AnimationSpirale2 extends AbstractAnimation {
 			int i = ecranInt.getColMax()/2-k;
 			for(int j=i+1;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(i, j, ecranFin.getColorByXY(i,j));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 			for(int j=i+1;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(j, ecranInt.getColMax()-i, ecranFin.getColorByXY(j,ecranInt.getColMax()-i));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 			for(int j=i+1;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(ecranInt.getColMax()-i, ecranInt.getColMax()-j, ecranFin.getColorByXY(ecranInt.getColMax()-i,ecranInt.getColMax()-j));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 			for(int j=i+1;j<ecranInt.getColMax()-i+1;j++){
 				ecranInt.updateColorByXY(ecranInt.getColMax()-j, i, ecranFin.getColorByXY(ecranInt.getColMax()-j,i));
-				this.wait(ecranInt.getColMax()/(2*(2*k+1)));
+				this.wait(1);
 				this.showScreen(ecranInt);
 			}
 		}
